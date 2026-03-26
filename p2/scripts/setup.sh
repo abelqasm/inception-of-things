ecf668e164b1234641985dd05f41a0965d4874a5
abelqasm <abderrahmane.belqasmi@gmail.com>
2026-03-26 20:00:53 +0100

    fix: check kubectl Api avalability insted of k3s service start

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode=644 --flannel-iface=eth1
-until sudo rc-service k3s status | grep -q started; do
-    sleep 2
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+until kubectl get nodes >/dev/null 2>&1; do
+    echo "Waiting for Kubernetes API..."
+    sleep 3
 done
 
-kubectl apply -f "/vagrant/app1/conf.yaml"
-kubectl apply -f "/vagrant/app2/conf.yaml"
-kubectl apply -f "/vagrant/app3/conf.yaml"
-kubectl apply -f "/vagrant/ingress.yaml"
+kubectl apply -n kube-system -f "/vagrant/app1/conf.yaml"
+kubectl apply -n kube-system -f "/vagrant/app2/conf.yaml"
+kubectl apply -n kube-system -f "/vagrant/app3/conf.yaml"
+kubectl apply -n kube-system -f "/vagrant/ingress.yaml"
