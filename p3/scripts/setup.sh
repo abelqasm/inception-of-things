4eede2fc56e42bd36c1d4cb0a32a3f92335a5041
abelqasm <abderrahmane.belqasmi@gmail.com>
2026-03-27 18:28:49 +0100

    add: ingress to map to argocd dashboard

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -13,6 +13,17 @@ kubectl create namespace dev
 
 sleep 3
 
-#  install argocd
+#  install argocd and wait to be ready
 echo "---------- Installing argocd into cluster ---------"
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+echo "---------- wait for argocd pods ---------"
+kubectl wait -n argocd --for=condition=Ready pods --all
+echo "---------- argocd is up and running ---------"
+sleep 3
+
+# apply Ingress
+
+echo "---------- Applying the Ingres ---------"
+kubectl apply -f ../confs/ingress.yaml
