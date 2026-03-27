119d5bd4005ee32229257eb5def78049324758a6
abelqasm <abderrahmane.belqasmi@gmail.com>
2026-03-27 17:39:44 +0100

    fix: remove the only sudo can call kubectl

--- p3/scripts/install-dependencies.sh
+++ p3/scripts/install-dependencies.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-
 set -e
 
-echo "===== Installing dependencies ====="
+echo "---------- Installing dependencies ----------"
 
-# Remove unecessary packages
-sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
-
-# Add Docker's official GPG key:
+sudo apt remove -y docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc || true
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+# Docker GPG key
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-tee /etc/apt/sources.list.d/docker.sources <<EOF
+# Docker repo
+sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -24,21 +22,29 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
-
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Enable & start Docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# Add current user to docker group
 sudo usermod -aG docker $USER
+
 # Install Kubectl
+
+echo "---------- Installing kubectl ----------"
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+rm -f kubectl kubectl.sha256
 
 # Install k3d
+echo "---------- Installing k3d ----------"
+
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-set -e
-
-# check Docker first
-if ! docker info >/dev/null 2>&1; then
-    echo "Docker daemon is not running or accessible"
-    exit 1
-fi
-
-echo "---------- Creating k3d cluster ---------"
+#  create cluster
+echo "---------- Creating k3d cluster  ----------"
 k3d cluster create p3 -p 8080:80@loadbalancer
 
 sleep 10
 
-echo "---------- Configuring Kubectl ---------"
-export KUBECONFIG=$(k3d kubeconfig write p3)
-
+#  create namespaces
 echo "---------- Creating argocd and dev namespaces ---------"
 kubectl create namespace argocd
 kubectl create namespace dev
 
 sleep 3
 
+#  install argocd
 echo "---------- Installing argocd into cluster ---------"
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
